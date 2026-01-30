57e09366acc5b284e87a5a8b8c2ce2c5908aa8d9
MuX123 <112720519+MuX123@users.noreply.github.com>
2026-01-30 23:38:05 +0800

    re
    re

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/CivisOS/' : '/',
+  // GitHub Pages 使用相對路徑
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
